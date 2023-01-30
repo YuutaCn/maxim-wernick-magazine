3f13008ef885ce339bab5fcc1f4d30f72ea82cff
yuutacn <yuutacn@gmail.com>
2023-01-30 15:11:24 +0300

    fix class name & feat: add hovers

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Поиск
-  document.querySelector('.search__btn').addEventListener('click', () => {
+  document.querySelector('.search__button').addEventListener('click', () => {
     if (window.screen.availWidth < 1024) {
       document.querySelector('.header__title').classList.toggle('visually-hidden');
     };
-    document.querySelector('.header__search').classList.toggle('active');
-    document.querySelector('.search__result').classList.remove('active');
+    document.querySelector('.header__search').classList.toggle('search_active');
+    document.querySelector('.search__result').classList.remove('result_active');
   });
 
   document.querySelector('.search__input').addEventListener('input', () => {
     let inputData = document.querySelector('.search__input').value.valueOf();
     let searchZone = document.querySelector('.search__result');
     if (inputData === '') {
-      searchZone.classList.remove('active');
+      searchZone.classList.remove('result_active');
     } else if (inputData.length > 0) {
-        searchZone.classList.add('active');
+        searchZone.classList.add('result_active');
       };
   });
 });
