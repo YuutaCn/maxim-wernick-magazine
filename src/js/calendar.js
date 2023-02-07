4561f551157896c387768327b2e0e8198de1ddaa
yuutacn <yuutacn@gmail.com>
2023-02-07 18:35:47 +0300

    calendar

---
+++ src/js/calendar.js
@@ -0,0 +1,11 @@
+new HelloWeek({
+  selector: '.calendar',
+  lang: 'ru',
+  todayHighlight: false,
+  format: 'DD/MM/YYYY',
+  weekStart: 0,
+  monthShort: false,
+  weekShort: true,
+  disablePastDays: false,
+  range: true,
+});
